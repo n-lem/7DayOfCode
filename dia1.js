3d2089f9fe7a36eedf25907cdc951b972b769657
Nahuel Lemos <nahuel.lemos.com>
2024-08-15 16:52:45 -0300

    Ejercicio original

---
+++ dia1.js
@@ -0,0 +1,43 @@
+// Tu tarea de hoy es reescribir el código a continuación para que imprima la información de manera correcta, que tenga sentido y sin errores:
+
+
+let numeroUn = 1
+let stringUn = '1'
+
+let numeroTreinta = 30
+let stringTreinta = '30'
+
+let numeroDiez = 10
+let stringDiez = '10'
+
+if (COMPARAR numeroUn y stringUn) {
+  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+} else {
+  console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+}
+
+
+if (COMPARAR numeroTreinta y stringTreinta) {
+  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
+} else {
+  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
+}
+
+if (COMPARAR numeroDiez y stringDiez) {
+  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
+} else {
+  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
+}
+
+//  CONSEJO  
+
+// También puedes utilizar el navegador para ejecutar este programa, si aún no tienes familiaridad con editores de código como Visual Studio Code.
+
+ 
+
+// Para hacerlo, como mencioné antes, basta con hacer clic con el botón derecho del mouse en cualquier página, seleccionar la opción “Inspeccionar”, ir a la pestaña “Consola” y escribir tu código. ¡Muy simple, verdad?
+
+ 
+
+// Si deseas cambiar los nombres de las variables y los valores, siéntete libre de hacerlo. Pero nunca imprimas algo que no sea verdadero, ¿eh?
+
